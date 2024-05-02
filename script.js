830720b94dc6135d42023e02586679975434576d
MaximilianPepper <164947441+MaximilianPepper@users.noreply.github.com>
2024-05-02 23:53:35 +0200

    Second commit All features are done expect grey to black with alpha values Minor styling adjustmenet still missing.

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const container = document.querySelector(".container");
-let side = 20;
+var colors = ['red', 'green', 'blue', 'orange', 'yellow'];
+let side = 40;
 let slider = document.querySelector("#range");
+let active = "black";
+let size = document.querySelector(".square-size");
+
+
+//buttons init
+const black = document.querySelector("#black");
+const random = document.querySelector("#random");
+const eraser = document.querySelector("#eraser");
+const reset = document.querySelector("#reset");
+const grey = document.querySelector("#grey");
+
 
 function generate(){
     side = slider.value ;
+    size.textContent = `${side}x${side}`;
     while(container.firstChild) {
         container.removeChild(container.firstChild);
     }
@@ -18,12 +31,58 @@ function generate(){
         
     }
 }
+generate();
+black.classList.add("active");
 
 slider.addEventListener("input", generate);
-generate();
 
+function alpha(target){
+    let style = getComputedStyle(element);
+            let bgColor = style.getPropertyValue('background-color');
+            let colors = bgColor.split(', ');
+            if (parseFloat(colors[3]) < 1) colors[3] = parseFloat(colors[3]) + 0.1;
+            element.style.backgroundColor = colors.join(', ');
+}
 document.addEventListener("mouseover", function(event){
     if (event.target.classList.contains('cell')){
-        event.target.classList.add('black');
+        if (active==="black") event.target.style.backgroundColor = "black"; 
+        if (active==="random") event.target.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+        if (active==="eraser") event.target.style.backgroundColor = "white";
+        if (active==="grey") event.target.style.backgroundColor = alpha(event.target);
     }
 });
+
+//buttons logic
+black.addEventListener("click", ()=>
+{
+    active="black"
+    black.classList.add("active");
+    random.classList.remove("active");
+    eraser.classList.remove("active");
+    grey.classList.remove("active");
+});
+eraser.addEventListener("click", ()=>
+{
+    active="eraser";
+    eraser.classList.add("active");
+    random.classList.remove("active");
+    black.classList.remove("active");
+    grey.classList.remove("active");
+});
+random.addEventListener("click", ()=>
+{
+    active="random";
+    random.classList.add("active");
+    eraser.classList.remove("active");
+    black.classList.remove("active");
+    grey.classList.remove("active");
+});
+grey.addEventListener("click",()=>
+{
+    active="grey";
+    grey.classList.add("active");
+    random.classList.remove("active");
+    eraser.classList.remove("active");
+    black.classList.remove("active");
+})
+reset.addEventListener("click",generate);
