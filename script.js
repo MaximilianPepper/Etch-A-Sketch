06febe5c503ff46843e91ed7e9e46ea3479a6511
MaximilianPepper <164947441+MaximilianPepper@users.noreply.github.com>
2024-05-03 00:05:54 +0200

    Final Version TODO: revisit styling

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ function generate(){
         for (let j = 0; j< side; j++){
             let cell = document.createElement("div");
             cell.classList.add('cell');
+            cell.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
             row.appendChild(cell);
         }
         container.appendChild(row);
@@ -37,17 +38,17 @@ black.classList.add("active");
 slider.addEventListener("input", generate);
 
 function alpha(target){
-    let style = getComputedStyle(element);
+    let style = getComputedStyle(target);
             let bgColor = style.getPropertyValue('background-color');
             let colors = bgColor.split(', ');
             if (parseFloat(colors[3]) < 1) colors[3] = parseFloat(colors[3]) + 0.1;
-            element.style.backgroundColor = colors.join(', ');
+            target.style.backgroundColor = colors.join(', ');
 }
 document.addEventListener("mouseover", function(event){
     if (event.target.classList.contains('cell')){
         if (active==="black") event.target.style.backgroundColor = "black"; 
         if (active==="random") event.target.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-        if (active==="eraser") event.target.style.backgroundColor = "white";
+        if (active==="eraser") event.target.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
         if (active==="grey") event.target.style.backgroundColor = alpha(event.target);
     }
 });
